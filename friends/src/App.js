37ddee61db4d9d9fb0298de563fb93634fc28b29
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-17 16:55:44 -0500

    rendered ProtectedRoute.
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 //components
-import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './components/Login.jsx'
+import FriendsList from './components/FriendsList'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Login/>
+        <Route path='/login' component={Login} />
+        <ProtectedRoute path='/' component={FriendsList} />
       </div>
     </Router>
   );
