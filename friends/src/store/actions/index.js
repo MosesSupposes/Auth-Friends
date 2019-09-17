53ad96f96aabe8a6eabdf3eeb6d717e853863595
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-17 16:51:41 -0500

    exported login actions.
    Co-authored-by: Moses Samuel <mosamuel98@gmail.com>

---
+++ friends/src/store/actions/index.js
@@ -0,0 +1,13 @@
+import {  
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    login
+} from './login'
+
+export {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    login
+}
