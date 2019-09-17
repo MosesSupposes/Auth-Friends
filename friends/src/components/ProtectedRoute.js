3b4627769d1f937508c80be2508367e71cb33c96
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-17 15:28:03 -0500

    added ProtectedRoute.
    Co-authored-by Moses Samuel <mosamuel98@gmail.com>

---
+++ friends/src/components/ProtectedRoute.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Route from 'react-router-dom'
+
+export default function ProtectedRoute({ component: Component,  ...rest  }) {
+    // Private Route rules:
+    // 1. It has the same API as <Route />.
+    // 2. It renders a <Route /> and passes all the props through to it.
+    // 3. It checks if the user is authenticated, if they are, it renders the “component” prop. If not, it redirects the user to /login.
+    return (
+        <Route 
+            {...rest}
+            render={props => (
+                localStorage.getItem('token')
+                    ? <Component />
+                    : <Redirect to='/login' />
+            )}
+        />
+    )
+    
+    
+}
