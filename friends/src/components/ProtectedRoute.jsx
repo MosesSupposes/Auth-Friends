13c901fac91a7eb47ce9097a041407164d01fdb0
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-17 17:05:34 -0700

    add a stricter auth check in ProtectedRoute

--- friends/src/components/ProtectedRoute.jsx
+++ friends/src/components/ProtectedRoute.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function ProtectedRoute({ component: Component,  ...rest  }) {
     // Private Route rules:
     // 1. It has the same API as <Route />.
     // 2. It renders a <Route /> and passes all the props through to it.
     // 3. It checks if the user is authenticated, if they are, it renders the “component” prop. If not, it redirects the user to /login.
+
+    const { authToken  } = useSelector(state => ({
+        authToken: state.login.authToken
+    }))
+
     return (
         <Route 
             {...rest}
             render={props => {
-                // TODO: check against authToken in redux store
-            //    return localStorage.getItem('token') === props.authToken
-               return localStorage.getItem('token')
+               return (localStorage.getItem('token') === authToken) 
                     ? <Component {...props} />
                     : <Redirect to='/login' />
             }}
